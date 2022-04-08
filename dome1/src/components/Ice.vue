a3575ab5f18d6733a79d3effa4502f280042c3bf
8300110 <1209821217@qq.com>
2022-04-08 17:37:59 +0800

    1209821217@qq.com

--- dome1/src/components/Ice.vue
+++ dome1/src/components/Ice.vue
@@ -1,43 +1,55 @@
 <template>
-    <div class="ice">
-        瑞纳冰
-         <ul class="ice1">
-            <li class="list1"  v-for="(k,index) in arrList" :key="index">
-                <img :src="k.img" alt="" class="pic">
-                <div class="box1">
-                <p class="name">{{k.name}}</p><br>
-                <p class="biaoqian">{{k.title}}</p>
-                <span class="money">{{k.money}}</span><span class="moneys">{{k.moneys}}</span>
-                <img :src="k.img1" alt="" class="jia">
-                </div>
-            </li>
-        </ul>
-    </div>
+  <div class="ice">
+    瑞纳冰
+    <ul class="ice1">
+      <li
+        class="list1"
+        v-for="(k, index) in arrList"
+        @click="Btn(k.id)"
+        :key="index"
+      >
+        <img :src="k.img" alt="" class="pic" />
+        <div class="box1">
+          <p class="name">{{ k.name }}</p>
+          <br />
+          <p class="biaoqian">{{ k.title }}</p>
+          <span class="money">{{ k.money }}</span
+          ><span class="moneys">{{ k.moneys }}</span>
+          <img :src="k.img1" alt="" class="jia" />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <style>
 @import url("../assets/css/ice.css");
 </style>
 <script>
-import {Test1} from '../request/api.js'
+import { Test1 } from "../request/api.js";
 export default {
-    data(){
-        return{
-            arrList:[]
-        }
+  data() {
+    return {
+      arrList: []
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    Btn(id) {
+      this.$router.push({
+        path: "/Instant",
+        query: { Instant: id }
+      });
     },
-     mounted(){
-        this.getData() 
-        },
-      methods:{
-       getData(){
-        var _this=this;
-        Test1().then(function(res){
-            // res就是请求成功之后的数据
-            _this.arrList=res.data.data6
-            console.log(res)
-        })
-       },
-       
-   }
-}
+    getData() {
+      var _this = this;
+      Test1().then(function(res) {
+        // res就是请求成功之后的数据
+        _this.arrList = res.data.data6;
+        console.log(res);
+      });
+    }
+  }
+};
 </script>
